479a9309c1a04b00b49b6b4ef143fd97a760b4a3
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-06-06 09:02:28 -0400

    Set flyway owner to elicit_owner

--- postgresql/init_scripts/V0.0.3__CREATE_TEST_SCHEMAS.sql
+++ postgresql/init_scripts/V0.0.3__CREATE_TEST_SCHEMAS.sql
@@ -2,10 +2,10 @@
 CREATE DATABASE survey_test;
 \c survey_test
 
-CREATE SCHEMA survey AUTHORIZATION survey_owner;
+CREATE SCHEMA survey AUTHORIZATION elicit_owner;
 GRANT USAGE on SCHEMA survey to survey_user;
 
-CREATE SCHEMA surveyreport AUTHORIZATION surveyreport_owner;
+CREATE SCHEMA surveyreport AUTHORIZATION elicit_owner;
 GRANT USAGE on SCHEMA surveyreport to surveyreport_user;
 GRANT USAGE on SCHEMA surveyreport to surveyadmin_user;
 GRANT USAGE on SCHEMA surveyreport to survey_user;
