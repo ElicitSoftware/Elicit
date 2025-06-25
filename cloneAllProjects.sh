3cc040bd88008b4aca4b23f7c29bb6054f5d74a3
Matthew Demerath <m.demerath@elicitsoftware.com>
2025-06-25 09:45:08 -0400

    Added script to help setup the project structure

---
+++ cloneAllProjects.sh
@@ -0,0 +1,6 @@
+# Clone all the projects into this directory
+# This setup allows the build script to run from the root of the repository
+git clone git@github.com:ElicitSoftware/Admin.git Admin
+git clone git@github.com:ElicitSoftware/FHHS.git FHHS
+git clone git@github.com:ElicitSoftware/Pedigree.git Pedigree
+git clone git@github.com:ElicitSoftware/Survey.git Survey
